110c0c03f4e6a391696fe20f5d5ac68fdaf0e6cd
Jim Talarski <jim.talarski@gmail.com>
2020-08-18 08:41:06 -0500

    attempt to use VS code

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
 $( document ).ready( readyNow );
 
 const budget = 25000;
-
 let purchases = [];
 
 function addPurchase() {
@@ -17,7 +16,42 @@ function addPurchase() {
    $( '#purchaseNameIn' ).val('');
    $( '#purchasePriceIn' ).val('');
   // calculate remaining budget
+  calculateRemaingBudget ();
+  // update DOM
+  displayPurchases();
 } // end addPurchase
+
+function calculateRemaingBudget() {
+  console.log('in calculateRemaingBudget');
+  // loop through purchases array
+  let totalPrices = 0;
+  for (i = 0; i< purchases.length; i++) {
+    // for each purchase, add up total of all prices
+    totalPrices += Number(purchases [ i ].price);
+  } // end for loop
+  console.log( 'totalPrices: ', totalPrices );
+  // subtract totalPrices from budget for remainingBudget
+  const remainingBudget = budget - totalPrices;
+  // display remainingBudget
+  let el = $( '#remainingBudgetOut' );
+  el.empty();
+  el.append( remainingBudget);
+} // end calculateRemaingBudget
+
+function displayPurchases() {
+  console.log( ' in displayPurchases' );
+  // target output by id
+  let el = $( '#purchasesOut' );
+  // empty
+  el.empty();
+  // loop through purchases array
+  for ( purchase of purchases ) {
+    // for each purchase, create a list item
+    el.append( `<li>` + purchase.name + `: $` + purchase.price +`</li>` );
+  } // end for loop
+} //end displayPurchases
+
+
 
 function readyNow () {
   // display budget
@@ -27,4 +61,6 @@ function readyNow () {
   el.append( budget );
   // handle click event
   $( '#addPurchaseButton' ).on( 'click', addPurchase );
+  //init display
+  calculateRemaingBudget();
 } // end readyNow
